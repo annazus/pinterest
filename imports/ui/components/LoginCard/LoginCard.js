19907023a9317f13e8d2fdebf01ef8fb29b3f86c
annazus <suzanna@appliedar.us>
2019-02-25 19:02:49 -0800

    Added validation to login

--- imports/ui/components/LoginCard/LoginCard.js
+++ imports/ui/components/LoginCard/LoginCard.js
@@ -1,46 +1,82 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { Meteor } from "meteor/meteor";
+import { timingSafeEqual } from "crypto";
 class LoginCard extends Component {
   constructor(props) {
     super(props);
-    this.state = { email: "", password: "", loggedIn: false, logginIn: false };
+    this.state = {
+      email: "",
+      password: "",
+      loggedIn: false,
+      logginIn: false,
+      errors: []
+    };
   }
 
-  onChange = (evt, input) => {
-    console.log(input);
-    console.log(this.state);
+  validate = () => {
+    let err = [];
+    console.log(this.state.errors);
+    this.setState({ errors: this.state.errors.splice(0) });
 
-    this.setState({ ...this.state, [input]: evt.target.value });
+    console.log(err);
+
+    if (this.state.email.trim() === "") {
+      console.log("email");
+
+      err.push("Email is required.");
+      console.log(err);
+    } else {
+      var re = /\S+@\S+\.\S+/;
+      if (!re.test(this.state.email)) {
+        err.push("Email is invalid.");
+      }
+    }
+    if (this.state.password.trim() === "") {
+      err.push("Password is required.");
+    }
+    this.setState({ errors: err });
+    console.log(err.length);
+    if (err.length > 0) {
+      console.log(" errors");
+      return false;
+    }
+    return true;
+  };
+  onChange = evt => {
+    this.setState({ [evt.target.name]: evt.target.value });
   };
 
   login = e => {
-    this.setState({ ...this.state, loggingIn: true });
     e.preventDefault();
     console.log("login");
+    if (!this.validate()) {
+      console.log("returnign");
+
+      return;
+    }
+    this.setState({ loggingIn: true });
 
     Meteor.loginWithPassword(
       this.state.email,
       this.state.password,
 
       err => {
-        console.log("siccess");
-        console.log(this.state);
         if (err) {
           console.log(err);
+          this.setState({ errors: this.state.errors.concat(err.reason) });
+
+          this.setState({ loggedIn: false, loggingIn: false });
           return;
         }
-        console.log("siccess");
-        console.log(this.state);
-        this.setState({ ...this.state, loggedIn: true, loggingIn: false });
+
+        this.setState({ loggedIn: true, loggingIn: false });
       }
     );
   };
 
   render() {
-    console.log(Meteor.userId());
-    console.log(Meteor.loggingIn());
-    console.log(this.state);
+    console.log(this.state.errors);
     return !this.state.loggingIn ? (
       this.state.loggedIn ? (
         <Redirect to="/" />
@@ -49,17 +85,26 @@ class LoginCard extends Component {
           <header>
             <img src="/images/logo.svg" />
             <h1>Welcome back!</h1>
+            <ul className="errors">
+              {this.state.errors.map(item => (
+                <li>{item}</li>
+              ))}
+            </ul>
           </header>
           <form>
             <input
               type="email"
+              name="email"
+              value={this.state.email}
               placeholder="Email"
-              onChange={e => this.onChange(e, "email")}
+              onChange={e => this.onChange(e)}
             />
             <input
               type="password"
+              name="password"
+              value={this.state.password}
               placeholder="Password"
-              onChange={e => this.onChange(e, "password")}
+              onChange={e => this.onChange(e)}
             />
             <button onClick={this.login}>Continue</button>
           </form>
